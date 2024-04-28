fd11acf55d36d6a78098fbb24efe5928cfd03df3
konfortes <konfortes@gmail.com>
2024-04-28 18:10:23 +0300

    Add webhook verify endpoint

---
+++ README.md
@@ -0,0 +1,31 @@
+# strava-webhooks-js
+
+## Strava
+
+### Webhook
+
+```bash
+# get current subscription
+curl -G https://www.strava.com/api/v3/push_subscriptions \
+    -d client_id=14xxx \
+    -d client_secret=a9xxxxxxxxxxxxxxxx
+
+# delete subscription
+curl -X DELETE "https://www.strava.com/api/v3/push_subscriptions/242807?client_id=14xxx&client_secret=a9xxxxxxxxxxxxxxxx"
+
+# create subscription
+curl -X POST https://www.strava.com/api/v3/push_subscriptions \
+      -F client_id=14xxx \
+      -F client_secret=a9xxxxxxxxxxxxxxxx \
+      -F 'callback_url=https://f9db-77-124-180-192.ngrok-free.app/webhook' \
+      -F 'verify_token=542xxxxxxxxxxxx'
+```
+
+## ngrok
+
+```bash
+# get token from [ngrok dashboard](https://dashboard.ngrok.com/)
+ngrok authtoken YOUR_TOKEN
+
+ngrok http 3001
+```
--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,37 @@ dotenv.config()
 const app: Express = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server')
-    console.debug(req.url)
+const verifyToken = process.env.STRAVA_VERIFY_TOKEN
+
+app.get('/health', (_req: Request, res: Response) => {
+    res.send('OK')
 })
+
+app.route('/webhook').get((req: Request, res: Response) => {
+    const mode = req.query['hub.mode']
+    const token = req.query['hub.verify_token']
+    const challenge = req.query['hub.challenge']
+
+    if (token !== verifyToken) {
+        console.log('Invalid verification token')
+        res.sendStatus(401)
+        return
+    }
+
+    if (mode === 'subscribe') {
+        // Responds with the challenge token from the request
+        console.log('webhook verified')
+
+        res.status(200).json({ 'hub.challenge': challenge })
+    } else {
+        console.log('unknown webhook mode')
+
+        res.sendStatus(403)
+    }
+})
+// .post((req: Request, res: Response) => {
+//     console.log(req.body)
+// })
 
 app.listen(port, () => {
     console.info(`[server]: Server is running at http://localhost:${port}`)
